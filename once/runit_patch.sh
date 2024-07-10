093d98411f1210d04b453d6bfef5111b179c7db0
Kerberoge <sjoerdenjh@gmail.com>
2024-07-11 00:30:07 +0200

    Added script that patches runit scripts

---
+++ once/runit_patch.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# This script makes some small modifications to runit scripts, in order to customize
+# their behavior to my liking.
+
+# disable /var/log/dmesg.log
+sed -i '/dmesg >\/var\/log\/dmesg.log/,+6d' /etc/runit/1
+
+# modify PATH to include binaries in /usr/local/bin in service scripts
+sed -i 's|PATH=/usr/bin:/usr/sbin|PATH=/usr/local/bin:/usr/bin|' /etc/runit/{1,2,3,ctrlaltdel}
