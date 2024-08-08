117a3ac61c096a86d497ddb2d8f210df1a1a28f4
Kerberoge <sjoerdenjh@gmail.com>
2024-08-08 23:28:38 +0200

    Added emptydirs patch

--- once/runit_patch.sh
+++ once/runit_patch.sh
@@ -8,3 +8,6 @@ sed -i '/dmesg >\/var\/log\/dmesg.log/,+6d' /etc/runit/1
 
 # modify PATH to include binaries in /usr/local/bin in service scripts
 sed -i 's|PATH=/usr/bin:/usr/sbin|PATH=/usr/local/bin:/usr/bin|' /etc/runit/{1,2,3,ctrlaltdel}
+
+# prevent the creation of useless directories
+patch -d /etc/runit/core-services < emptydirs.patch
